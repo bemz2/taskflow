ee50c64fe61dd7ab32d55f5f9664bfe53fa17f60
Vitaliy Kharitonov <rueke23@gmail.com>
2026-02-26 16:10:38 +0300

    Добавил методы для работы с бд

--- internal/repository/task.go
+++ internal/repository/task.go
@@ -2,13 +2,17 @@ package repository
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
 	sq "github.com/Masterminds/squirrel"
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+var ErrTaskNotFound = errors.New("task not found")
 
 type TaskModel struct {
 	ID          uuid.UUID  `db:"id"`
@@ -48,40 +52,39 @@ func (r *TaskRepository) CreateTask(ctx context.Context, t TaskModel) (TaskModel
 		return TaskModel{}, fmt.Errorf("%s: failed to build sql: %w", contextKey, err)
 	}
 
-	var created TaskModel
+	var task TaskModel
 
 	err = r.db.QueryRow(ctx, query, args...).
 		Scan(
-			&created.ID,
-			&created.UserID,
-			&created.Title,
-			&created.Description,
-			&created.Status,
-			&created.CreatedAt,
-			&created.CompletedAt,
+			&task.ID,
+			&task.UserID,
+			&task.Title,
+			&task.Description,
+			&task.Status,
+			&task.CreatedAt,
+			&task.CompletedAt,
 		)
 	if err != nil {
 		return TaskModel{}, fmt.Errorf("%s: failed to query row: %w", contextKey, err)
 	}
 
-	return created, err
+	return task, nil
 }
 
 func (r *TaskRepository) ListTasks(ctx context.Context, userID uuid.UUID) ([]TaskModel, error) {
 	const contextKey = "TaskRepository.ListTasks"
 
-	query := sq.
+	query, args, err := sq.
 		Select("id", "user_id", "title", "description", "status", "created_at", "completed_at").
 		From("tasks").
 		Where(sq.Eq{"user_id": userID}).
-		PlaceholderFormat(sq.Dollar)
+		PlaceholderFormat(sq.Dollar).ToSql()
 
-	sqlStr, args, err := query.ToSql()
 	if err != nil {
 		return nil, fmt.Errorf("%s: failed to build sql %w", contextKey, err)
 	}
 
-	rows, err := r.db.Query(ctx, sqlStr, args...)
+	rows, err := r.db.Query(ctx, query, args...)
 	if err != nil {
 		return nil, fmt.Errorf("%s: failed to query: %w", contextKey, err)
 	}
@@ -90,36 +93,153 @@ func (r *TaskRepository) ListTasks(ctx context.Context, userID uuid.UUID) ([]Tas
 	var tasks []TaskModel
 
 	for rows.Next() {
-		var t TaskModel
+		var task TaskModel
 		err = rows.Scan(
-			&t.ID,
-			&t.UserID,
-			&t.Title,
-			&t.Description,
-			&t.Status,
-			&t.CreatedAt,
-			&t.CompletedAt,
+			&task.ID,
+			&task.UserID,
+			&task.Title,
+			&task.Description,
+			&task.Status,
+			&task.CreatedAt,
+			&task.CompletedAt,
 		)
-		tasks = append(tasks, t)
+		tasks = append(tasks, task)
 	}
 	if err := rows.Err(); err != nil {
 		return nil, fmt.Errorf("%s: failed scan: %w", contextKey, err)
 	}
-	return tasks, err
+	return tasks, nil
 }
 
-func (r *TaskRepository) GetTask(ctx context.Context) {
+func (r *TaskRepository) GetTask(ctx context.Context, id, userID uuid.UUID) (TaskModel, error) {
+	const contextKey = "TaskRepository.GetTask"
 
+	query := sq.
+		Select("id", "user_id", "title", "description", "status", "created_at", "completed_at").
+		From("tasks").
+		Where(sq.Eq{
+			"id":      id,
+			"user_id": userID}).
+		PlaceholderFormat(sq.Dollar)
+
+	sqlStr, args, err := query.ToSql()
+	if err != nil {
+		return TaskModel{}, fmt.Errorf("%s: failed to build sql: %w", contextKey, err)
+	}
+
+	var task TaskModel
+
+	err = r.db.QueryRow(ctx, sqlStr, args...).Scan(
+		&task.ID,
+		&task.UserID,
+		&task.Title,
+		&task.Description,
+		&task.Status,
+		&task.CreatedAt,
+		&task.CompletedAt,
+	)
+	if errors.Is(err, pgx.ErrNoRows) {
+		return TaskModel{}, ErrTaskNotFound
+	}
+
+	if err != nil {
+		return TaskModel{}, fmt.Errorf("%s: failed to query row: %w", contextKey, err)
+	}
+
+	return task, nil
 }
 
-func (r *TaskRepository) DeleteTask(ctx context.Context) {
+func (r *TaskRepository) DeleteTask(ctx context.Context, id, userID uuid.UUID) error {
+	const contextKey = "TaskRepository.DeleteTask"
 
+	query, args, err := sq.Delete("tasks").
+		Where(sq.Eq{
+			"id":      id,
+			"user_id": userID}).
+		PlaceholderFormat(sq.Dollar).
+		ToSql()
+
+	if err != nil {
+		return fmt.Errorf("%s: failed to build sql: %w", contextKey, err)
+	}
+
+	result, err := r.db.Exec(ctx, query, args...)
+	if err != nil {
+		return fmt.Errorf("%s: exec delete: %w", contextKey, err)
+	}
+	affected := result.RowsAffected()
+	if affected == 0 {
+		return ErrTaskNotFound
+	}
+
+	return nil
 }
 
-func (r *TaskRepository) UpdateTask(ctx context.Context) {
+func (r *TaskRepository) UpdateTask(ctx context.Context, t TaskModel) (TaskModel, error) {
+	const contextKey = "TaskRepository.UpdateTask"
 
+	query, args, err := sq.
+		Update("tasks").
+		Set("title", t.Title).
+		Set("description", t.Description).
+		Set("status", t.Status).
+		Set("completed_at", t.CompletedAt).
+		Where(sq.Eq{
+			"id":      t.ID,
+			"user_id": t.UserID,
+		}).
+		Suffix("RETURNING id, user_id, title, description, status, created_at, completed_at").
+		PlaceholderFormat(sq.Dollar).
+		ToSql()
+	if err != nil {
+		return TaskModel{}, fmt.Errorf("%s: build sql: %w", contextKey, err)
+	}
+
+	var updated TaskModel
+
+	err = r.db.QueryRow(ctx, query, args...).Scan(
+		&updated.ID,
+		&updated.UserID,
+		&updated.Title,
+		&updated.Description,
+		&updated.Status,
+		&updated.CreatedAt,
+		&updated.CompletedAt,
+	)
+
+	if errors.Is(err, pgx.ErrNoRows) {
+		return TaskModel{}, ErrTaskNotFound
+	}
+
+	if err != nil {
+		return TaskModel{}, fmt.Errorf("%s: scan: %w", contextKey, err)
+	}
+
+	return updated, nil
 }
 
-func (r *TaskRepository) UpdateTaskStatus(ctx context.Context) {
+func (r *TaskRepository) UpdateTaskStatus(ctx context.Context, id, userID uuid.UUID, status string) error {
+	const contextKey = "TaskRepository.UpdateTaskStatus"
 
+	query, args, err := sq.Update("tasks").
+		Set("status", status).
+		Where(sq.Eq{
+			"id":      id,
+			"user_id": userID}).
+		PlaceholderFormat(sq.Dollar).
+		ToSql()
+	if err != nil {
+		return fmt.Errorf("%s: build sql: %w", contextKey, err)
+	}
+
+	result, err := r.db.Exec(ctx, query, args...)
+	if err != nil {
+		return fmt.Errorf("%s: exec update task status: %w", contextKey, err)
+	}
+	affected := result.RowsAffected()
+	if affected == 0 {
+		return ErrTaskNotFound
+	}
+
+	return nil
 }
--- internal/service/task.go
+++ internal/service/task.go
@@ -9,9 +9,9 @@ import (
 
 type TaskRepository interface {
 	CreateTask(ctx context.Context, t repository.TaskModel) (repository.TaskModel, error)
-	GetTask(ctx context.Context, id uuid.UUID, userID uuid.UUID) (repository.TaskModel, error)
+	GetTask(ctx context.Context, id, userID uuid.UUID) (repository.TaskModel, error)
 	ListTasks(ctx context.Context, userID uuid.UUID) ([]repository.TaskModel, error)
 	UpdateTask(ctx context.Context, t repository.TaskModel) (repository.TaskModel, error)
-	DeleteTask(ctx context.Context, id uuid.UUID, userID uuid.UUID) error
-	UpdateTaskStatus(ctx context.Context, id uuid.UUID, userID uuid.UUID, status string) error
+	DeleteTask(ctx context.Context, id, userID uuid.UUID) error
+	UpdateTaskStatus(ctx context.Context, id, userID uuid.UUID, status string) error
 }
